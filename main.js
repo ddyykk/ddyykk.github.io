ecf9394b492ee9dedc0c09f2e3fa96b95a0c53ea
Johnson Du <42371034+ddyykk@users.noreply.github.com>
2021-01-11 15:34:21 +0800

    New page for the first Javascript code

---
+++ main.js
@@ -0,0 +1,6 @@
+function myFunction(){
+    document.getElementById("demo").innerHTML=Date();
+    document.getElementById("demo").style.fontSize = "25px"; 
+    document.getElementById("demo").style.color = "red";
+    document.getElementById("demo").style.backgroundColor = "white";  
+}
