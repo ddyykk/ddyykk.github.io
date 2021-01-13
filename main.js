ab8747fcbb6ae37fd1abc1e84db02951b0375d13
Johnson Du <42371034+ddyykk@users.noreply.github.com>
2021-01-13 12:47:32 +0800

    Change the Javascript to external file

--- main.js
+++ main.js
@@ -4,3 +4,16 @@ function myFunction(){
     document.getElementById("demo").style.color = "red";
     document.getElementById("demo").style.backgroundColor = "white";  
 }
+
+function changeStyle(sw) {
+    if(sw==0) {
+        document.getElementById("demo2").style.color="green";
+        document.getElementById("demo2").style.backgroundColor="pink";
+        document.getElementById("demo2").style.fontSize="5vw";
+    }
+    else{
+        document.getElementById("demo2").style.color="red";
+        document.getElementById("demo2").style.backgroundColor="cyan";
+        document.getElementById("demo2").style.fontSize="6vw";
+    }
+}
