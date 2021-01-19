98dc90a407e27d541bfd63668b17efb166334720
Johnson Du <42371034+ddyykk@users.noreply.github.com>
2021-01-19 21:14:03 +0800

    Add code format to make them easy to read

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ddyykk.github.io
- The first web develop demo project I am going to build.
- I can learn how to construct a website step-by-step.
- The first stage is to make some static web pages that uses the basic tags and attributes of the HTML.
- 
+
+The first web develop demo project I am going to build.
+I can learn how to construct a website step-by-step.
+The first stage is to make some static web pages that uses the basic tags and attributes of the HTML.
--- main.js
+++ main.js
@@ -1,19 +1,18 @@
-function myFunction(){
-    document.getElementById("demo").innerHTML=Date();
-    document.getElementById("demo").style.fontSize = "25px"; 
-    document.getElementById("demo").style.color = "red";
-    document.getElementById("demo").style.backgroundColor = "white";  
+function myFunction() {
+    document.getElementById('demo').innerHTML = Date();
+    document.getElementById('demo').style.fontSize = '25px';
+    document.getElementById('demo').style.color = 'red';
+    document.getElementById('demo').style.backgroundColor = 'white';
 }
 
 function changeStyle(sw) {
-    if(sw==0) {
-        document.getElementById("demo2").style.color="green";
-        document.getElementById("demo2").style.backgroundColor="pink";
-        document.getElementById("demo2").style.fontSize="5vw";
-    }
-    else{
-        document.getElementById("demo2").style.color="red";
-        document.getElementById("demo2").style.backgroundColor="cyan";
-        document.getElementById("demo2").style.fontSize="6vw";
+    if (sw == 0) {
+        document.getElementById('demo2').style.color = 'green';
+        document.getElementById('demo2').style.backgroundColor = 'pink';
+        document.getElementById('demo2').style.fontSize = '5vw';
+    } else {
+        document.getElementById('demo2').style.color = 'red';
+        document.getElementById('demo2').style.backgroundColor = 'cyan';
+        document.getElementById('demo2').style.fontSize = '6vw';
     }
 }
