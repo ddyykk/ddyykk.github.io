9894c1b529ff53de1c0b74d03bc70e06c2e07d81
Johnson Du <42371034+ddyykk@users.noreply.github.com>
2021-04-02 17:11:16 +0800

    Combine the code of javascript to the calculator

---
+++ JS/calculator.js
@@ -0,0 +1,33 @@
+let calculator = {
+
+    finish_bit:true,
+    // finish bit is to see if the calculation is finished, if finished, then start a new one. True means the calculation is done.
+    clear: function () {
+        document.getElementById('display').textContent = 0;
+        // clear the display
+        this.finish_bit=true;
+    },
+
+    get: function (value) {
+        if (this.finish_bit) {
+            document.getElementById('display').textContent = '';
+            this.finish_bit=false;
+            // clear the zero in the display and enable the calculation by set the finish bit to false.
+        } else {
+            this.finish_bit=false;
+        }
+        document.getElementById('display').textContent += value;
+        // this append the input to a string
+    },
+
+    calculate: function () {
+        return eval(document.getElementById('display').textContent);
+        // the line will use the input string as a math instruction and return the answer
+    },
+
+    show: function () {
+        document.getElementById('display').textContent = this.calculate();
+        this.finish_bit=true;
+        // This function display the answer and reset the finish bit.
+    },
+};
