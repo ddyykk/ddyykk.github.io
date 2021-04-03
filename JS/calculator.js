243c5dc5ca54b8bbc20d7a1486078a1dcc102c9b
Johnson Du <42371034+ddyykk@users.noreply.github.com>
2021-04-03 09:10:16 +0800

    Fine tune the layout

--- JS/calculator.js
+++ JS/calculator.js
@@ -1,31 +1,30 @@
 let calculator = {
-
-    finish_bit:true,
+    finish_bit: true,
     // finish bit is to see if the calculation is finished, if finished, then start a new one. True means the calculation is done.
     clear: function () {
         document.getElementById('display').textContent = 0;
         // clear the display
-        this.finish_bit=true;
+        this.finish_bit = true;
     },
 
     get: function (value) {
         if (this.finish_bit) {
             document.getElementById('display').textContent = '';
-            this.finish_bit=false;
+            this.finish_bit = false;
             // clear the zero in the display and enable the calculation by set the finish bit to false.
         } else {
-            this.finish_bit=false;
+            this.finish_bit = false;
         }
         document.getElementById('display').textContent += value;
         // this append the input to a string
     },
-    del:function(){
+    del: function () {
         if (this.finish_bit) {
             // if one calculation is just finished, del button does nothing to it
         } else {
-            let a=document.getElementById('display').innerHTML.toString();
-            a=a.substr(0,a.length-1);
-            document.getElementById('display').innerHTML=a;
+            let a = document.getElementById('display').innerHTML.toString();
+            a = a.substr(0, a.length - 1);
+            document.getElementById('display').innerHTML = a;
         }
     },
     calculate: function () {
@@ -35,7 +34,7 @@ let calculator = {
 
     show: function () {
         document.getElementById('display').textContent = this.calculate();
-        this.finish_bit=true;
+        this.finish_bit = true;
         // This function display the answer and reset the finish bit.
     },
 };
